2fb383db06b8cc2a2e877acac85f0779a3a5f43f
Raph <rbleuzet@gmail.com>
2015-12-25 20:43:47 +0100

    update config

--- config.php
+++ config.php
@@ -6,20 +6,24 @@
  * @package  Config
  * @author   bleuze_r <raphael.bleuzet@epitech.eu>
  * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://localhost:800/config.php
+ * @link     config.php
  */
-/*
-SERVER CONFIGURATION
-*/
-$host   = 'localhost'; // host
-$port   = '3306'; // port
-$name   = 'framework'; // BDD NAME
-$user   = 'root'; // Login BDD
-$pass   = ''; // Password BDD
-/* $socket = '/home/bleuze_r/.mysql/mysql.sock'; */
-/*  */
-
-/*
-HASH SALT
-*/
-$salt = 'fnifeg1er65 4q12 q56s31 g 65q3r1 2';
+return [
+	'databases' => [
+		'default' => [
+			'host' => 'localhost',
+			'port' => '3306',
+			'dbname' => 'database',
+			'username' => 'root',
+			'password' => ''
+		],
+		'tests' => [
+			'host' => 'localhost',
+			'port' => '3306',
+			'dbname' => 'database-test',
+			'username' => 'test',
+			'password' => 'test'
+		]
+	],
+	'salt' => 'please change salt',
+];
